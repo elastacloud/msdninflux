297e4072ab92abc14a8022961d8a04f713305e15
Andy Cross <andy@elastacloud.com>
2015-03-17 18:08:46 +0000

    first commit

---
+++ msdninfluxdb/Program.cs
@@ -0,0 +1,53 @@
+﻿using InfluxDB.Net;
+using InfluxDB.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msdninfluxdb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            Console.WriteLine("Running. Press ANY key to exit");
+            Console.ReadKey();
+        }
+
+        private static async void Run()
+        {
+            try
+            {
+                var _client = new InfluxDb("http://influxmsdn.cloudapp.net:8086", "root", "root");
+
+                var rand = new Random();
+                for (int i = 1; i < 100; i++)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        Serie serie = new Serie.Builder("cpustatus")
+                                        .Columns("machine", "cpuusage")
+                                        .Values("Machine" + j, rand.Next(0, i))
+                                        .Build();
+                        InfluxDbApiResponse writeResponse = await _client.WriteAsync("msdncpus", TimeUnit.Milliseconds, serie);
+                        if (writeResponse.Success)
+                        {
+                            Console.WriteLine("Data Point added " + (i*100+j).ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("unable to write point!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ msdninfluxdbquery/Program.cs
@@ -0,0 +1,38 @@
+﻿using InfluxDB.Net;
+using InfluxDB.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msdninfluxdbquery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+            Console.ReadKey();
+        }
+
+        private static async void Run()
+        { 
+            var _client = new InfluxDb("http://influxmsdn.cloudapp.net:8086", "root", "root");
+            List<Serie> series = await _client.QueryAsync("msdncpus", "select mean(cpuusage) from cpustatus where machine =~ /Machine.*0/i group by time(1m)", TimeUnit.Milliseconds);
+
+            foreach (var item in series)
+	        {
+                Console.WriteLine(item.Name);
+                foreach (var point in item.Points)
+	            {
+                    for (int i = 0; i < item.Columns.Length; i++)
+                    {
+                        Console.WriteLine(item.Columns[i] +":" + point[i].ToString());
+                    }
+                    Console.WriteLine("=====================");
+	            }
+	        }
+        }
+    }
+}
